d39b7d4818199db39d6654b7212c511ca3c21fe7
Cyril Bernadet <cyril.bernadet@gmail.com>
2021-01-20 22:23:35 +0100

    Initialisation du projet Spring Boot

---
+++ src/test/java/com/kinikun/drafter/loldrafterapi/LolDrafterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinikun.drafter.loldrafterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolDrafterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
