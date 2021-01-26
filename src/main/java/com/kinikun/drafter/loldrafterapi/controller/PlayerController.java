9b9a9c6877345ff928678c3bb9b42428e9709a3f
Cyril Bernadet <cyril.bernadet@gmail.com>
2021-01-26 23:37:59 +0100

    Ajout d'un controlleur REST pour les players

---
+++ src/main/java/com/kinikun/drafter/loldrafterapi/controller/PlayerController.java
@@ -0,0 +1,42 @@
+package com.kinikun.drafter.loldrafterapi.controller;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import com.kinikun.drafter.loldrafterapi.batch.entity.PlayerEntity;
+import com.kinikun.drafter.loldrafterapi.batch.repository.PlayerRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlayerController {
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    @RequestMapping("/players")
+    public Iterable<PlayerEntity> findAllPlayers() {
+        return this.playerRepository.findAll();
+    }
+
+    @RequestMapping("/players/{id}")
+    public ResponseEntity<PlayerEntity> findPlayer(@PathVariable String id) {
+        try {
+            UUID uuid = UUID.fromString(id);
+            Optional<PlayerEntity> player = this.playerRepository.findById(uuid);
+
+            if (player.isPresent()) {
+                return new ResponseEntity<>(player.get(), HttpStatus.FOUND);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
